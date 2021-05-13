8645eea0b5f3853b4677916cc158437c4e59abf6
Ernesto Borio <ernestoborio@gmail.com>
2021-05-12 23:14:26 -0300

    Added Palette to ZXRenderer

--- src/ZXRenderer.gd
+++ src/ZXRenderer.gd
@@ -1,19 +1,44 @@
 extends ViewportContainer
 
+#class_name ZXRenderer
+
+export var palette: PoolColorArray = [
+	Color(0,0,0), # Black
+	Color(0,0,1), # Blue
+	Color(1,0,0), # Red
+	Color(1,0,1), # Magenta
+	Color(0,1,0), # Green
+	Color(0,1,1), # Cyan
+	Color(1,1,0), # Yellow
+	Color(1,1,1)  # White
+	]
+
 func _ready():
-	var attr_ink = PoolByteArray([])
-	attr_ink.resize(32*24)
+	set_palette(palette)
+	_init_attributes("ink", 7)
+	_init_attributes("paper", 0)
+
+func _init_attributes(attribute_plane, default_color):
+	var attributes = PoolByteArray([])
+	attributes.resize(32*24)
 	for i in range(0, 32*24):
-		attr_ink[i] = i % 8 # fill array with [0..7]
+		attributes[i] = default_color
 	
 	var image = Image.new()
-	image.create_from_data(32, 24, false, Image.FORMAT_R8, attr_ink)
+	image.create_from_data(32, 24, false, Image.FORMAT_R8, attributes)
 
-	var ink = ImageTexture.new()
-	ink.create_from_image(image, 0)
+	var attr_texture = ImageTexture.new()
+	attr_texture.create_from_image(image, 0)
 	
-	(self.material as ShaderMaterial).set_shader_param("ink", ink)
-	
-	
-	
-	
+	material.set_shader_param(attribute_plane, attr_texture)
+
+func set_palette(colors):
+	var image = Image.new()
+	image.create(colors.size(), 1, false, Image.FORMAT_RGB8)
+	image.lock()
+	for i in range(0, colors.size()):
+		image.set_pixel(i, 0, colors[i])
+	image.unlock()
+	var texture = ImageTexture.new()
+	texture.create_from_image(image, 0)
+	material.set_shader_param("palette", texture)
--- src/ZXRenderer.shader
+++ src/ZXRenderer.shader
@@ -4,12 +4,16 @@ const vec4 white = vec4(1., 1., 1., 1.);
 const vec4 black = vec4(0., 0., 0., 1.);
 
 uniform sampler2D ink;
+uniform sampler2D paper;
+uniform sampler2D palette;
 
 void fragment() {
+	// bool is_ink = texture(TEXTURE, UV).r >= 1.; // quick check for non-black pixel
 	vec4 color = texture(TEXTURE, UV);
 	ivec2 cell = ivec2(UV / TEXTURE_PIXEL_SIZE) / 8;
-	float ink_color = texelFetch(ink, cell, 0).r;
+	float color_attr = texelFetch(ink, cell, 0).r;
 	COLOR = black;
+	// if(is_ink) 
 	if(color == white) {
 		COLOR.r = ink_color * (256. / 8.);
 	}
