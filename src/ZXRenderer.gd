0d558f05adf6633e1fd86a2f7e4a176a30e35cd4
Ernesto Borio <ernestoborio@gmail.com>
2021-05-17 23:17:27 -0300

    Palette and ink & paper attributes working!

--- src/ZXRenderer.gd
+++ src/ZXRenderer.gd
@@ -1,6 +1,11 @@
 extends ViewportContainer
 
-#class_name ZXRenderer
+class_name ZXRenderer
+
+# How many cells wide the screen is
+const CELL_WIDTH = 32
+# How many cells tall the screen is
+const CELL_HEIGHT = 24
 
 export var palette: PoolColorArray = [
 	Color(0,0,0), # Black
@@ -13,24 +18,28 @@ export var palette: PoolColorArray = [
 	Color(1,1,1)  # White
 	]
 
+var attr_ink: PoolByteArray
+var attr_paper: PoolByteArray
+
 func _ready():
 	set_palette(palette)
-	_init_attributes("ink", 7)
-	_init_attributes("paper", 0)
+	attr_ink = _init_attributes("ink", 7)
+	attr_paper = _init_attributes("paper", 0)
 
-func _init_attributes(attribute_plane, default_color):
-	var attributes = PoolByteArray([])
-	attributes.resize(32*24)
-	for i in range(0, 32*24):
+func _init_attributes(attribute_plane: String, default_color: int):
+	var attributes: PoolByteArray = PoolByteArray([])
+	attributes.resize(CELL_WIDTH * CELL_HEIGHT)
+	for i in range(0, CELL_WIDTH * CELL_HEIGHT):
 		attributes[i] = default_color
 	
 	var image = Image.new()
-	image.create_from_data(32, 24, false, Image.FORMAT_R8, attributes)
+	image.create_from_data(CELL_WIDTH, CELL_HEIGHT, false, Image.FORMAT_R8, attributes)
 
 	var attr_texture = ImageTexture.new()
 	attr_texture.create_from_image(image, 0)
 	
 	material.set_shader_param(attribute_plane, attr_texture)
+	return attributes
 
 func set_palette(colors):
 	var image = Image.new()
@@ -42,3 +51,24 @@ func set_palette(colors):
 	var texture = ImageTexture.new()
 	texture.create_from_image(image, 0)
 	material.set_shader_param("palette", texture)
+
+func set_attribute(attribute_plane: String, x: int, y: int, color: int):
+	var index: int = x + y * CELL_WIDTH
+	if attribute_plane == "paper":
+		attr_paper.set(index, color)
+	else:
+		attr_ink.set(index, color)
+
+# Sends an attribute plane to the shader
+func _shade_attributes(attribute_plane: String, attributes: PoolByteArray):
+	var image = Image.new()
+	image.create_from_data(CELL_WIDTH, CELL_HEIGHT, false, Image.FORMAT_R8, attributes)
+
+	var attr_texture = ImageTexture.new()
+	attr_texture.create_from_image(image, 0)
+	
+	material.set_shader_param(attribute_plane, attr_texture)
+
+func _process(_delta):
+	_shade_attributes("ink", attr_ink)
+	_shade_attributes("paper", attr_paper)
--- src/ZXRenderer.shader
+++ src/ZXRenderer.shader
@@ -1,20 +1,24 @@
 shader_type canvas_item;
 
-const vec4 white = vec4(1., 1., 1., 1.);
-const vec4 black = vec4(0., 0., 0., 1.);
+const int CELL_WIDTH = 32;
+const int CELL_HEIGHT = 24;
+const int COLOR_COUNT = 12;
 
 uniform sampler2D ink;
 uniform sampler2D paper;
 uniform sampler2D palette;
 
 void fragment() {
-	// bool is_ink = texture(TEXTURE, UV).r >= 1.; // quick check for non-black pixel
-	vec4 color = texture(TEXTURE, UV);
+	bool is_ink = texture(TEXTURE, UV).r >= 1.; // quick check for non-black pixel
 	ivec2 cell = ivec2(UV / TEXTURE_PIXEL_SIZE) / 8;
-	float color_attr = texelFetch(ink, cell, 0).r;
-	COLOR = black;
-	// if(is_ink) 
-	if(color == white) {
-		COLOR.r = ink_color * (256. / 8.);
+	cell.y = CELL_HEIGHT - 1 - cell.y; // invert Y to get top left to be the origin
+	
+	float color_attr;
+	if(is_ink) {
+		color_attr = texelFetch(ink, cell, 0).r;
 	}
+	else {
+		color_attr = texelFetch(paper , cell, 0).r;
+	}
+	COLOR = texture(palette, vec2(color_attr * (256. / float(COLOR_COUNT)), 0.));
 }
