7d632b42f6c798747c4dd1edaf539d3f9d850f34
Ernesto Borio <ernestoborio@gmail.com>
2021-06-19 17:11:02 -0300

    WIP. Things were done.

--- src/ZXRenderer.gd
+++ src/ZXRenderer.gd
@@ -21,10 +21,12 @@ export var palette: PoolColorArray = [
 var attr_ink: PoolByteArray
 var attr_paper: PoolByteArray
 
+
 func _ready():
 	set_palette(palette)
-	attr_ink = _init_attributes("ink", 4)
-	attr_paper = _init_attributes("paper", 3)
+	attr_ink = _init_attributes("ink", 5)
+	attr_paper = _init_attributes("paper", 0)
+
 
 func _init_attributes(attribute_plane: String, default_color: int):
 	var attributes: PoolByteArray = PoolByteArray([])
@@ -41,6 +43,7 @@ func _init_attributes(attribute_plane: String, default_color: int):
 	material.set_shader_param(attribute_plane, attr_texture)
 	return attributes
 
+
 func set_palette(colors):
 	var image = Image.new()
 	image.create(colors.size(), 1, false, Image.FORMAT_RGB8)
@@ -52,12 +55,14 @@ func set_palette(colors):
 	texture.create_from_image(image, 0)
 	material.set_shader_param("palette", texture)
 
+
 func set_attribute(attribute_plane: String, x: int, y: int, color: int):
 	var index: int = x + y * CELL_WIDTH
 	if attribute_plane == "paper":
 		attr_paper.set(index, color)
 	else:
 		attr_ink.set(index, color)
+
 
 # Sends an attribute plane to the shader
 func _shade_attributes(attribute_plane: String, attributes: PoolByteArray):
@@ -69,6 +74,8 @@ func _shade_attributes(attribute_plane: String, attributes: PoolByteArray):
 	
 	material.set_shader_param(attribute_plane, attr_texture)
 
+	
 func _process(_delta):
 	_shade_attributes("ink", attr_ink)
 	_shade_attributes("paper", attr_paper)
+	
---
+++ src/ZXprite.gd
@@ -0,0 +1,6 @@
+extends Sprite
+
+class_name ZXprite, "res://Graphics/ZXprite.svg"
+
+export var paper: int = 0
+export var ink: int = 7
